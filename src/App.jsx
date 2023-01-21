8c935878b6ec4a4a9289caad97ae985b0fa93ddd
unkerpete <peterkjx@gmail.com>
2023-01-21 12:53:20 +0800

    Playing around with css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const [recipe, setRecipe] = useState('recipe placeholder');
   const [userName, setUserName] = useState('Peter');
-  const [input, setInput] = useState();
+  // const [input, setInput] = useState();
 
   const handleClickAPI = async function () {
     if (recipe === 'recipe placeholder') {
@@ -28,15 +28,24 @@ function App() {
 
   return (
     <div className='App'>
-      Hello, {userName} <br />
-      <input type='text' onChange={handleInput} />
-      <br />
-      <button onClick={handleClickAPI}>
-        Click Me and recipe of Old Fashioned should appear below if api is
-        working
-      </button>
-      <br />
-      {recipe}
+      <h1>Cocktail Recipes</h1>
+      <div className='top-section-container'>
+        <div className='top-section-left'>
+          I AM TOP SECTION LEFT Hello, {userName} <br />
+        </div>
+        <div className='top-section-right-container'>
+          <div className='top-section-right'>I am top section right</div>
+        </div>
+      </div>
+      <div className='search-section'>
+        <input type='text' onChange={handleInput} />
+        <br />
+        <button onClick={handleClickAPI}>
+          Click Me and recipe of Old Fashioned should appear below if api is
+          working
+        </button>
+      </div>
+      <div className='results-display'>{recipe} recipe results go here</div>
     </div>
   );
 }
